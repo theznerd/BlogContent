0651236e34e74147e70fd04589f3fd1e01e42ea3
Nathan Ziehnert <nathan@z-nerd.com>
2021-01-21 23:41:08 -0700

    Delete 2021/01/TSPauser/TSPauser directory

--- 2021/01/TSPauser/TSPauser/OSDProcess.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TSPauser
-{
-    public class OSDProcess
-    {
-        public string UIName { get; set; }
-        public int? PID { get; set; }
-        public string ProcessName { get; set; }
-    }
-}
