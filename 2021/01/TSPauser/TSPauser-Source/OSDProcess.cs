05a50d10cf20e43120e5cf0f5ab4b2770abe4c91
Nathan Ziehnert <nathan@z-nerd.com>
2021-01-21 23:41:53 -0700

    Add files via upload

---
+++ 2021/01/TSPauser/TSPauser-Source/OSDProcess.cs
@@ -0,0 +1,9 @@
+﻿namespace TSPauser
+{
+    public class OSDProcess
+    {
+        public string UIName { get; set; }
+        public int? PID { get; set; }
+        public string ProcessName { get; set; }
+    }
+}
